cba83dd6b94b5edfc551508926fdd30a16916181
emjay6 <69481318+emjay6@users.noreply.github.com>
2024-09-04 13:33:06 -0400

    Uploaded my test

---
+++ myscript.sh
@@ -0,0 +1,14 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2761
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red7\green68\blue184;\red0\green0\blue0;\red218\green76\blue12;
+\red14\green14\blue14;\red6\green33\blue79;}
+{\*\expandedcolortbl;;\cssrgb\c0\c36078\c77255;\cssrgb\c0\c0\c0\c3137;\cssrgb\c89020\c38431\c3529;
+\cssrgb\c6667\c6667\c6667;\cssrgb\c1176\c18431\c38431;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs28 \cf2 \cb3 \expnd0\expndtw0\kerning0
+#!/bin/bash\
+\pard\pardeftab720\partightenfactor0
+\cf4 \cb3 echo\cf5  \cf6 "Hello, World!"}
